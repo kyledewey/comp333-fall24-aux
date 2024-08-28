fe894c7f03b53c05c05333bca66ac09ab0ed7000
Kyle Dewey <kyle.dewey@csun.edu>
2024-08-28 12:34:03 -0700

    abstracted over writing

--- section_1/java/Example.java
+++ section_1/java/Example.java
@@ -3,19 +3,37 @@
 public class Example {
     // public static boolean doesUserWantConsole(String[] args) { ... }
     public static String getDestinationFile(String[] args) throws Exception { ... }
-    public static int doComputation() { return 0; }
+
+    public static void write(FileOutputStream stream, int toWrite) {
+	if (stream == null) { // odd - rechecking stream
+	    System.out.println(retval);
+	} else {
+	    stream.writeln(retval);
+	}
+    }
+	
+    public static int doComputation(FileOutputStream stream) {
+	int retval = 3;
+	for (long x = 1; x < 999999999999; x++) {
+	    retval *= (int)x;
+	    if (x % 1000 == 0) {
+		write(stream, retval); // odd - rechecking stream
+	    }
+	}
+	return retval;
+    }
     
     public static void main(String[] args) throws Exception {
 	String destinationFile = getDestinationFile(args);
+	FileOutputStream stream = null;
+	if (destinationFile != null) {
+	    stream = new FileOutputStream(destinationFile);
+	}
 
-	int result = doComputation();
+	int result = doComputation(stream);
 
-	if (destinationFile == null) {
-	    System.out.println(result);
-	} else {
-	    File file = new File(destinationFile);
-	    FileOutputStream stream = new FileOutputStream(file);
-	    stream.writeln(result);
+	write(stream, result);
+	if (stream != null) {
 	    stream.close();
 	}
     }
