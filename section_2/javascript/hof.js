3e3a5a49947606cff9adb6c80e691c43c7d061a1
Kyle Dewey <kyle.dewey@csun.edu>
2024-10-14 17:20:51 -0700

    .

---
+++ section_2/javascript/hof.js
@@ -0,0 +1,73 @@
+// higher-order functions
+// closures
+// anonymous functions
+// lambda functions
+// lambdas
+
+function printArray(arr) {
+    for (let index = 0; index < arr.length; index++) {
+        let element = arr[index];
+        console.log(element);
+    }
+}
+
+// shouldPrint is a function
+// - Takes an array element
+// - Returns true if we should print the array element
+function printSomeArray(arr, shouldPrint) {
+    for (let index = 0; index < arr.length; index++) {
+        let element = arr[index];
+        if (shouldPrint(element)) {
+            console.log(element);
+        }
+    }
+}
+
+function printGreaterThan(arr, amount) {
+    printSomeArray(arr, (arrElem) => arrElem > amount);
+                   // function (arrElem) {
+                   //     return arrElem > amount;
+                   // });
+}
+
+function printLessThan(arr, amount) {
+    printSomeArray(arr,
+                   function (arrElem) {
+                       return arrElem < amount;
+                   });
+}
+
+function printLessThanFive(arr) {
+    printLessThan(arr, 5);
+}
+
+function printLessThanTen(arr) {
+    printLessThan(arr, 10);
+}
+
+
+// function printLessOrGreaterThan(arr, amount, isLess) {
+//     for (let index = 0; index < arr.length; index++) {
+//         let element = arr[index];
+//         if (isLess) {
+//             if (element < amount) {
+//                 console.log(element);
+//             }
+//         } else {
+//             if (element > amount) {
+//                 console.log(element);
+//             }
+//         }
+//     }
+// }
+
+// higher-order function: functions are data
+
+
+function seeIncDec(x) {
+    return [function () { return x; },
+            function () { x++; },
+            function () { x--; }];
+}
+
+        
