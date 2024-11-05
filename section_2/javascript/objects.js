8a814a972ed989d6b4b22096fea638a53f5ec2d7
Kyle Dewey <kyle.dewey@csun.edu>
2024-11-05 15:48:22 -0800

    .

---
+++ section_2/javascript/objects.js
@@ -0,0 +1,11 @@
+// function makeRectangle(w, h) {
+//     return { 'width': w, 'height': h };
+// }
+
+function Rectangle(w, h) {
+    // if called as a constructor (new Rectangle(..., ...))
+    // this = {};
+    this.width = w;
+    this.height = h;
+    // return this;
+}
