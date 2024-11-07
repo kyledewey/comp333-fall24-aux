4ef87179765f68888b1c34ef69244649dffd05a8
Kyle Dewey <kyle.dewey@csun.edu>
2024-11-06 17:13:46 -0800

    .

--- section_2/javascript/objects.js
+++ section_2/javascript/objects.js
@@ -2,10 +2,19 @@
 //     return { 'width': w, 'height': h };
 // }
 
+const rectanglePrototype = {
+    'getArea': function () {
+        return this.width * this.height;
+    }
+};
+
+// Preferred way: put prototype directly on Rectangle
+// TODO: show this
 function Rectangle(w, h) {
     // if called as a constructor (new Rectangle(..., ...))
     // this = {};
     this.width = w;
     this.height = h;
+    this.__proto__ = rectanglePrototype;
     // return this;
 }
