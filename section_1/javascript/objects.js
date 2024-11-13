5c87420a5c24ed4611ca684cda392bd4d8c8d145
Kyle Dewey <kyle.dewey@csun.edu>
2024-11-13 12:54:29 -0800

    .

--- section_1/javascript/objects.js
+++ section_1/javascript/objects.js
@@ -1,10 +1,16 @@
 // FOR WEDNESDAY: finish showing constructors; explaining
 // code below
 function Rectangle(w, h) {
+    // if called as a constructor (with new, like new Rectangle...)
+    // this = {};
     this.width = w;
     this.height = h;
+    this.getArea = function () {
+        return this.width * this.height;
+    };
+    // return this;
 }
 
-// function makeRectangle(w, h) {
-//     return { 'width': w, 'height': h };
-// }
+function makeRectangle(w, h) {
+    return { 'width': w, 'height': h };
+}
